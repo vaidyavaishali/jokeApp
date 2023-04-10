2a7adeb6c1ce775eeaa0d290be4eead97c172a39
Vaishali Vaidya <vaishalivaidya1996@gmail.com>
2023-04-10 23:47:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
   const [joke, setJoke] = useState("")
   const [bookmark, setBookmark] = useState([])
   const AddJoke = () => {
@@ -16,17 +17,14 @@ function App() {
   }
 
   useEffect(() => {
-    const book = JSON.parse(localStorage.getItem('bookmark'))
-    if (book) {
-      setBookmark(book)
-    }
-  }, [])
-
-  useEffect(() => {
     localStorage.setItem("bookmark", JSON.stringify(bookmark))
   }, [bookmark])
 
   function AddBookmark(joke) {
+    const book = JSON.parse(localStorage.getItem('bookmark'))
+    if (book) {
+      setBookmark(book)
+    }
     if (bookmark[bookmark.length - 1] !== joke) {
       setBookmark([...bookmark, joke])
 
@@ -48,7 +46,6 @@ function App() {
   return (
     <div className='main-container'>
       <header>
-
         <h2>Joke Application</h2>
         <button onClick={AddJoke} id="add-joke">New Joke</button>
         <button onClick={() => { AddBookmark(joke) }} id="add-book">Add Bookmark</button>
@@ -61,23 +58,26 @@ function App() {
             return (
               <div key={i}>
                 <p>{book.setup} {book.punchline}</p>
-                <button onClick={() => { RemoveBookmark(i) }} id="rem-book">Remove Bookmark</button>
+                <button onClick={() => { RemoveBookmark(i) }} id="rem-book">Remove</button>
               </div>
             )
           })}
         </div>
-        <div id='joke-container' >
+        {joke == "" ? <h6>Click on add joke button to add the jokes</h6>: <div id='joke-container' >
           <p>{joke.setup}</p>
           <p>{joke.punchline}</p>
 
-        </div>
+        </div>}
+        
       </div >
       <footer>
         <p>Author: Vaishali Vaidya</p> 
         <p style={{ marginTop:"-23px"}}>
         <a href='https://github.com/vaidyavaishali/jokeApp' target='_blank'>
-            view Source code
+           click here
         </a>
+        <span> </span>
+       to view view Source code
         </p>
        
       </footer>
